dd813522977539268cafd7d39d2e03fcf8346a37
AlexRJS <alexrjs@gmail.com>
2016-04-19 21:59:58 +0200

    Latest changes;

--- library/callbacks/human_log.py
+++ library/callbacks/human_log.py
@@ -16,80 +16,102 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+OUT_FIELDS = ['cmd', 'command', 'start', 'end', 'delta', 'msg', 'stdout', 'stderr']
+
+def extract_fields(tags, vars):
+    for t in tags:
+        values = t.split('(')
+        marker = values[0]
+        if marker == 'human_log':
+            fields = values[1].replace(')', '').split('|')
+        for k,v in vars.iteritems():
+            # print "k,v:",k,v
+            if k == 'human_log':
+                for i in v:
+                    if i not in fields:
+                        fields.append(i)
     
-FIELDS = ['cmd', 'command', 'start', 'end', 'delta', 'msg', 'stdout', 'stderr']
-
-def human_log(res):
-
+    # print fields
+    return fields
+    
+def human_log(res, fields=None):
+    if not fields:
+        global OUT_FIELDS
+        fields = OUT_FIELDS
+        
     if type(res) == type(dict()):
-        for field in FIELDS:
+        for field in fields:
             if field in res.keys():
-            	# use default encoding, check out sys.setdefaultencoding
-                print u'\n{0}:\n{1}'.format(field, res[field])
+                # use default encoding, check out sys.setdefaultencoding
+                if field and res[field]:
+                    print u'\n{0}:\n{1}'.format(field, res[field])
                 # or use specific encoding, e.g. utf-8
                 #print '\n{0}:\n{1}'.format(field, res[field].encode('utf-8'))
 
 class CallbackModule(object):
 
-	def on_any(self, *args, **kwargs):
-		pass
+    def on_any(self, *args, **kwargs):
+        pass
 
-	def runner_on_failed(self, host, res, ignore_errors=False):
-		human_log(res)
+    def runner_on_failed(self, host, res, ignore_errors=False):
+        human_log(res)
 
-	def runner_on_ok(self, host, res):
-		human_log(res)
+    def runner_on_ok(self, host, res):
+        # print "tags:", self.task.tags
+        human_log(res, extract_fields(self.task.tags, self.task.play_vars))
 
-	def runner_on_error(self, host, msg):
-		pass
+    def runner_on_error(self, host, msg):
+        pass
 
-	def runner_on_skipped(self, host, item=None):
-		pass
+    def runner_on_skipped(self, host, item=None):
+        pass
 
-	def runner_on_unreachable(self, host, res):
-		human_log(res)
+    def runner_on_unreachable(self, host, res):
+        human_log(res)
 
-	def runner_on_no_hosts(self):
-		pass
+    def runner_on_no_hosts(self):
+        pass
 
-	def runner_on_async_poll(self, host, res, jid, clock):
-		human_log(res)
+    def runner_on_async_poll(self, host, res, jid, clock):
+        human_log(res)
 
-	def runner_on_async_ok(self, host, res, jid):
-		human_log(res)
+    def runner_on_async_ok(self, host, res, jid):
+        human_log(res)
 
-	def runner_on_async_failed(self, host, res, jid):
-		human_log(res)
+    def runner_on_async_failed(self, host, res, jid):
+        human_log(res)
 
-	def playbook_on_start(self):
-		pass
+    def playbook_on_start(self):
+        pass
 
-	def playbook_on_notify(self, host, handler):
-		pass
+    def playbook_on_notify(self, host, handler):
+        pass
 
-	def playbook_on_no_hosts_matched(self):
-		pass
+    def playbook_on_no_hosts_matched(self):
+        pass
 
-	def playbook_on_no_hosts_remaining(self):
-		pass
+    def playbook_on_no_hosts_remaining(self):
+        pass
 
-	def playbook_on_task_start(self, name, is_conditional):
-		pass
+    def playbook_on_task_start(self, name, is_conditional):
+        pass
 
-	def playbook_on_vars_prompt(self, varname, private=True, prompt=None, encrypt=None, confirm=False, salt_size=None, salt=None, default=None):
-		pass
+    def playbook_on_vars_prompt(self, varname, private=True, prompt=None, encrypt=None, confirm=False, salt_size=None, salt=None, default=None):
+        pass
 
-	def playbook_on_setup(self):
-		pass
+    def playbook_on_setup(self):
+        pass
 
-	def playbook_on_import_for_host(self, host, imported_file):
-		pass
+    def playbook_on_import_for_host(self, host, imported_file):
+        pass
 
-	def playbook_on_not_import_for_host(self, host, missing_file):
-		pass
+    def playbook_on_not_import_for_host(self, host, missing_file):
+        pass
 
-	def playbook_on_play_start(self, pattern):
-		pass
+    def playbook_on_play_start(self, pattern):
+        pass
 
-	def playbook_on_stats(self, stats):
-		pass
+    def playbook_on_stats(self, stats):
+        pass
