67dba8a99df3d0691bdb0e7cc966fd8c4d964d20
Alexander Schiessl <alexrjs@aol.com>
2017-02-11 16:40:03 +0100

    Refactored; Extracted atrifact to site

--- configs/makefiles/apps.mk
+++ configs/makefiles/apps.mk
@@ -1,11 +1,7 @@
 .PHONY: *
 
 
-verify_var_app:
-	$(if $(APP),,$(error [Error] APP not set!))
-
-
-define SET_EXTRA_APP
+define SET_APP_NAME
 	$(eval APP=$(1))
 	$(if $(APP),$(info [Info] Use app name: $(APP)),$(error [Error] APP not set!))
 	$(eval EXTRA_APP=-e "app_name=$(APP)")
@@ -14,6 +10,21 @@ define SET_EXTRA_APP
 	@true
 endef
 
+define SET_APP_ARTIFACT_INFO
+	$(eval ENV=$(1))
+	$(eval ARTIFACT_SPEC=libs-snapshot-local/$(2))
+	$(eval ARTIFACT_NAME=$(3))
+	$(call ENV_COMMON)
+endef
+
+
+verify_var_app:
+	$(if $(APP),,$(error [Error] APP not set!))
+
 
 jenkins: 
-	$(call SET_EXTRA_APP,$@)
+	$(call SET_APP_NAME,$@)
+
+site:
+	$(call SET_APP_NAME,$@)
+	$(call SET_APP_ARTIFACT_INFO,$@,sites,ci/current.tgz)
--- configs/makefiles/environments.mk
+++ configs/makefiles/environments.mk
@@ -2,19 +2,12 @@
 
 
 define ENV_COMMON
+	$(eval ENV=$(1))
 	$(if $(ENV),$(info [Info] Use environment: $(ENV)),$(error [Error] ENV not set!))
 	$(eval EXTRA_ENV=-e "env_name=$(ENV)")
 	$(info [Info] Use environment extra: $(EXTRA_ENV))
-	$(eval EXTRAS += -e "artifact_spec=$(ARTIFACT_SPEC)" -e "artifact_name=$(ARTIFACT_NAME)")
 	$(info [Info])
 	@true
-endef
-
-define SET_ENV_INFO
-	$(eval ENV=$(1))
-	$(eval ARTIFACT_SPEC=libs-snapshot-local/$(2))
-	$(eval ARTIFACT_NAME=$(3))
-	$(call ENV_COMMON)
 endef
 
 
@@ -22,10 +15,10 @@ verify_var_env:
 	$(if $(ENV),,$(error [Error] ENV not set!))
 
 try:
-	$(call SET_ENV_INFO,$@,sites,ci/current.tgz)
+	$(call ENV_COMMON,$@)
 
 develop:
-	$(call SET_ENV_INFO,$@,sites,ci/current.tgz)
+	$(call ENV_COMMON,$@)
 
 #$(eval ARTIFACT_SPEC=libs-release-local/sites)
 #v2.arjs.net.0.1.6.tgz
