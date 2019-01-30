dbae809239c2791aefe730661a2b601573e8f1d7
Alex Schiessl <alexrjs@gmail.com>
2019-01-30 11:04:20 +0100

    Updated html inventory script

--- inventory/html-inventory.py
+++ inventory/html-inventory.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+
 try:
     # For Python 3.0 and later
     from urllib.request import urlopen
@@ -11,52 +12,81 @@ import os
 import json
 import argparse
 
-
 def get_json_data(url):
     """Receive the content of ``url``, parse it as JSON and return the
        object.
     """
 
+    # print url
     response = urlopen(url)
+    # print response
     data = str(response.read())
+    # print data
+    # print json.loads(data)
     return json.loads(data)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--list", action="store_true")
-    parser.add_argument("--host")
-    parser.add_argument("--group")
-    parser.add_argument("--env")
+    group = parser.add_mutually_exclusive_group()
+    group.required = True
+    group.add_argument("--list", action="store_true")
+    group.add_argument("--host", nargs=1)
     args = parser.parse_args()
     # print args
-    if args.env:
-        jsond = get_json_data('http://ansible.cmdb.info/hosts/' + str(args.env))
+
+    data = {
+        '_meta': {
+            'hostvars': {
+            }
+        },
+        'all':
+        {
+            'children': [
+                'ungrouped'
+            ]
+        },
+        'ungrouped': {
+            'hosts': [                    
+            ]
+        }
+    }
+    if args.host:
+        jsond = get_json_data('http://ansible.cmdb.info:8098/hosts/?value=' + str(args.host))
         args.list = True if not args.host and not args.group else False
     else:
-        jsond = get_json_data('http://ansible.cmdb.info/hosts/' + str(os.path.basename(__file__)).replace('inv-', ''))
+        jsond = get_json_data('http://ansible.cmdb.info:8098/hosts/')
+        # print jsond
 
-    hosts = []
-    for k,v in jsond.items():
-        print k,v
-        if type(v) is list:
-            for h in v:
-                if h not in hosts:
-                    hosts.append(h)
-        elif type(v) is dict:
-            if 'hosts' in v:
-                for h in v['hosts']:
-                    if h not in hosts:
-                        hosts.append(h)
+        for _host in jsond:
+            _host_name = str(_host['host'])
+            data['ungrouped']['hosts'].append(_host_name)
+            data['_meta']['hostvars'][_host_name] = {}
+            for _vars in _host['vars']:
+                data['_meta']['hostvars'][_host_name][str(_vars)] = str(_host['vars'][_vars])
 
-    # print hosts
-    if args.list:
-        print json.dumps(jsond, indent=4)
-    elif args.host:
-        if args.host and args.host in hosts:
-            print '{{"hostname": "{}" }}'.format(args.host)
-        else:
-            print "{}"
-    elif args.group:
-        print '{{"{}": {} }}'.format(args.group, json.dumps(jsond[args.group], indent=4))
-    else:
-        print "{}"
+        jsond = get_json_data('http://ansible.cmdb.info:8098/groups/')
+        # print jsond
+
+        for _group in jsond:
+            _group_name = str(_group['name'])
+            if ':children' in _group_name:
+                _group_name = _group_name.replace(':children', '')
+                data[_group_name] = {
+                    'children': []
+                }
+                for _value in _group['value']:
+                    data[_group_name]['children'].append(str(_value))
+            else:
+                data[_group_name] = {
+                    'hosts': []
+                }
+                for _value in _group['value']:
+                    _host = str(_value)
+                    data[_group_name]['hosts'].append(_host)
+                    if _host in data['ungrouped']['hosts']:
+                        data['ungrouped']['hosts'].remove(_host)
+
+            data['all']['children'].append(_group_name)
+
+    print data
